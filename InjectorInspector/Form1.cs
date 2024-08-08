b03b3fb9138c79d8125a3015d950decb504f3474
smallbear999 <smallbear@finma.com.tw>
2024-08-08 14:37:28 +0800

    add button for testing

--- InjectorInspector/Form1.cs
+++ InjectorInspector/Form1.cs
@@ -20,5 +20,10 @@ namespace InjectorInspector
         private void button1_Click(object sender, EventArgs e)
         {
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
