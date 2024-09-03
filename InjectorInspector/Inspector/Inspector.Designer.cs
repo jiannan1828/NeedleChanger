3056f193f6df0ed6c3cf58dff243eacb335f368f
Xavier <jiannan1828@hotmail.com>
2024-09-03 15:25:16 +0800

    Revert "Temporarily remove other folders that affect the compiling result."
    This reverts commit 30c6e221d61628eba818c833b381383694af8a50.

--- InjectorInspector/Inspector/Inspector.Designer.cs
+++ InjectorInspector/Inspector/Inspector.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
+            this.Win1 = new VMControls.Winform.Release.VmRenderControl();
+            this.Win2 = new VMControls.Winform.Release.VmRenderControl();
+            this.Win3 = new VMControls.Winform.Release.VmRenderControl();
+            this.Win4 = new VMControls.Winform.Release.VmRenderControl();
+            this.Win5 = new VMControls.Winform.Release.VmRenderControl();
+            this.Win6 = new VMControls.Winform.Release.VmRenderControl();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,76 +46,100 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 5F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.panel3, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.panel5, 4, 1);
-            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.panel4, 2, 3);
-            this.tableLayoutPanel1.Controls.Add(this.panel6, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Win1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Win2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Win3, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Win4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Win5, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Win6, 4, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(907, 702);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1068, 702);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // panel1
+            // Win1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 23);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(292, 325);
-            this.panel1.TabIndex = 0;
+            this.Win1.BackColor = System.Drawing.Color.Black;
+            this.Win1.CoordinateInfoVisible = true;
+            this.Win1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Win1.ImageSource = null;
+            this.Win1.IsShowCustomROIMenu = false;
+            this.Win1.Location = new System.Drawing.Point(3, 33);
+            this.Win1.ModuleSource = null;
+            this.Win1.Name = "Win1";
+            this.Win1.Size = new System.Drawing.Size(346, 315);
+            this.Win1.TabIndex = 0;
             // 
-            // panel2
+            // Win2
             // 
-            this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 374);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(292, 325);
-            this.panel2.TabIndex = 1;
+            this.Win2.BackColor = System.Drawing.Color.Black;
+            this.Win2.CoordinateInfoVisible = true;
+            this.Win2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Win2.ImageSource = null;
+            this.Win2.IsShowCustomROIMenu = false;
+            this.Win2.Location = new System.Drawing.Point(360, 33);
+            this.Win2.ModuleSource = null;
+            this.Win2.Name = "Win2";
+            this.Win2.Size = new System.Drawing.Size(346, 315);
+            this.Win2.TabIndex = 1;
             // 
-            // panel3
+            // Win3
             // 
-            this.panel3.BackColor = System.Drawing.Color.Black;
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(306, 23);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(292, 325);
-            this.panel3.TabIndex = 2;
+            this.Win3.BackColor = System.Drawing.Color.Black;
+            this.Win3.CoordinateInfoVisible = true;
+            this.Win3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Win3.ImageSource = null;
+            this.Win3.IsShowCustomROIMenu = false;
+            this.Win3.Location = new System.Drawing.Point(717, 33);
+            this.Win3.ModuleSource = null;
+            this.Win3.Name = "Win3";
+            this.Win3.Size = new System.Drawing.Size(348, 315);
+            this.Win3.TabIndex = 2;
             // 
-            // panel4
+            // Win4
             // 
-            this.panel4.BackColor = System.Drawing.Color.Black;
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(306, 374);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(292, 325);
-            this.panel4.TabIndex = 3;
+            this.Win4.BackColor = System.Drawing.Color.Black;
+            this.Win4.CoordinateInfoVisible = true;
+            this.Win4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Win4.ImageSource = null;
+            this.Win4.IsShowCustomROIMenu = false;
+            this.Win4.Location = new System.Drawing.Point(3, 384);
+            this.Win4.ModuleSource = null;
+            this.Win4.Name = "Win4";
+            this.Win4.Size = new System.Drawing.Size(346, 315);
+            this.Win4.TabIndex = 3;
             // 
-            // panel6
+            // Win5
             // 
-            this.panel6.BackColor = System.Drawing.Color.Black;
-            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel6.Location = new System.Drawing.Point(609, 374);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(295, 325);
-            this.panel6.TabIndex = 5;
+            this.Win5.BackColor = System.Drawing.Color.Black;
+            this.Win5.CoordinateInfoVisible = true;
+            this.Win5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Win5.ImageSource = null;
+            this.Win5.IsShowCustomROIMenu = false;
+            this.Win5.Location = new System.Drawing.Point(360, 384);
+            this.Win5.ModuleSource = null;
+            this.Win5.Name = "Win5";
+            this.Win5.Size = new System.Drawing.Size(346, 315);
+            this.Win5.TabIndex = 4;
             // 
-            // panel5
+            // Win6
             // 
-            this.panel5.BackColor = System.Drawing.Color.Black;
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(609, 23);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(295, 325);
-            this.panel5.TabIndex = 6;
+            this.Win6.BackColor = System.Drawing.Color.Black;
+            this.Win6.CoordinateInfoVisible = true;
+            this.Win6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Win6.ImageSource = null;
+            this.Win6.IsShowCustomROIMenu = false;
+            this.Win6.Location = new System.Drawing.Point(717, 384);
+            this.Win6.ModuleSource = null;
+            this.Win6.Name = "Win6";
+            this.Win6.Size = new System.Drawing.Size(348, 315);
+            this.Win6.TabIndex = 5;
             // 
             // Inspector
             // 
@@ -123,7 +147,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Inspector";
-            this.Size = new System.Drawing.Size(907, 702);
+            this.Size = new System.Drawing.Size(1068, 702);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -132,11 +156,11 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel5;
+        private VMControls.Winform.Release.VmRenderControl Win1;
+        private VMControls.Winform.Release.VmRenderControl Win2;
+        private VMControls.Winform.Release.VmRenderControl Win3;
+        private VMControls.Winform.Release.VmRenderControl Win4;
+        private VMControls.Winform.Release.VmRenderControl Win5;
+        private VMControls.Winform.Release.VmRenderControl Win6;
     }
 }
---
+++ InjectorInspector/VisionTestFrm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace InjectorInspector
+{
+    partial class VisionTestFrm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.inspector1 = new Inspector.Inspector();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(1101, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "分析吸嘴影像";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.分析吸嘴影像_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(1101, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "分析Tray盤影像";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.分析Tray盤影像_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(1100, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "分析入料盤影像";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.分析入料盤影像_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(1259, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 54);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Setup TrayCCD";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.SetupCCDTray_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(1259, 36);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 54);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Setup 入料CCD";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.SetupCCD入料_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(1101, 406);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "批次分析入料盤";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.批次分析入料盤_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(1101, 466);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 54);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "批次分析Tray盤";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.批次分析Tray盤_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(1101, 526);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(152, 54);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "批次分析吸嘴";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.批次分析吸嘴_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(340, 331);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(670, 38);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Visible = false;
+            // 
+            // inspector1
+            // 
+            this.inspector1.Location = new System.Drawing.Point(14, 14);
+            this.inspector1.Margin = new System.Windows.Forms.Padding(5);
+            this.inspector1.Name = "inspector1";
+            this.inspector1.Size = new System.Drawing.Size(1078, 717);
+            this.inspector1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1423, 807);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inspector1);
+            this.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Inspector.Inspector inspector1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
