f3f037788121e41e3f93fd45e649f87afc1e0723
jiannan1828 <jiannan1828@hotmail.com>
2024-12-02 20:56:26 +0800

    add create, connect, button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,37 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
             this.inspector1 = new Inspector.Inspector();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_DeviceCreate = new System.Windows.Forms.Button();
+            this.btn_AlarmRST = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1236, 791);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.inspector1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1228, 758);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1143, 39);
+            this.button1.Location = new System.Drawing.Point(1062, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 83);
-            this.button1.TabIndex = 1;
+            this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // inspector1
             // 
             this.inspector1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.inspector1.Location = new System.Drawing.Point(14, 6);
+            this.inspector1.Location = new System.Drawing.Point(58, 7);
             this.inspector1.Margin = new System.Windows.Forms.Padding(5);
             this.inspector1.Name = "inspector1";
             this.inspector1.Size = new System.Drawing.Size(1112, 744);
-            this.inspector1.TabIndex = 0;
+            this.inspector1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btn_AlarmRST);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.btn_Connect);
+            this.tabPage2.Controls.Add(this.btn_DeviceCreate);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1228, 758);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(33, 88);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(165, 39);
+            this.btn_Connect.TabIndex = 1;
+            this.btn_Connect.Text = "btn_Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_DeviceCreate
+            // 
+            this.btn_DeviceCreate.Location = new System.Drawing.Point(33, 27);
+            this.btn_DeviceCreate.Name = "btn_DeviceCreate";
+            this.btn_DeviceCreate.Size = new System.Drawing.Size(165, 39);
+            this.btn_DeviceCreate.TabIndex = 0;
+            this.btn_DeviceCreate.Text = "btn_DeviceCreate";
+            this.btn_DeviceCreate.UseVisualStyleBackColor = true;
+            this.btn_DeviceCreate.Click += new System.EventHandler(this.btn_DeviceCreate_Click);
+            // 
+            // btn_AlarmRST
+            // 
+            this.btn_AlarmRST.Location = new System.Drawing.Point(33, 150);
+            this.btn_AlarmRST.Name = "btn_AlarmRST";
+            this.btn_AlarmRST.Size = new System.Drawing.Size(165, 39);
+            this.btn_AlarmRST.TabIndex = 3;
+            this.btn_AlarmRST.Text = "btn_AlarmRST";
+            this.btn_AlarmRST.UseVisualStyleBackColor = true;
+            this.btn_AlarmRST.Click += new System.EventHandler(this.btn_AlarmRST_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1232, 764);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.inspector1);
+            this.ClientSize = new System.Drawing.Size(1260, 815);
+            this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -66,15 +149,27 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Inspector.Inspector inspector1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button button1;
+        private Inspector.Inspector inspector1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_DeviceCreate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_AlarmRST;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//PCCP Xavier Tsai, added for testing <START>
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+using System.Xml;
+using WMX3ApiCLR;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+using System.Reflection.Emit;
+using System.Diagnostics;
+
+//WMX3
+using WMX3ApiCLR;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+using static WMX3ApiCLR.AdvMotion;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.CompilerServices;
+
+//JSON
+using System.IO;
+//using System.Text.Json;
+
+//TCP Server
+//Vibration
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices.ComTypes;
+//PCCP Xavier Tsai, added for testing <END>
+
 namespace InjectorInspector
 {
     public partial class Form1 : Form
@@ -26,5 +64,227 @@ namespace InjectorInspector
         {
             inspector1.xInit();
         }
+
+
+
+        //PCCP Xavier Tsai, added for testing <START>
+        public static WMX3Api Wmx3Lib = new WMX3Api();
+        public static EngineStatus EnStatus = new EngineStatus();
+        public static CoreMotionStatus CmStatus = new CoreMotionStatus();
+        public static CoreMotion Wmx3Lib_cm = new CoreMotion();//Wmx3Lib);
+        public static AdvancedMotion WmxLib_Adv = new AdvancedMotion();// Wmx3Lib);
+        public static CoreMotionAxisStatus[] cmAxis = new CoreMotionAxisStatus[8];
+        public System.Windows.Forms.NumericUpDown NUD_Motor_NO;
+
+        //WMX3
+        WMX3Api wmx = new WMX3Api();
+        //Io io = new Io();
+        CoreMotion motion = new CoreMotion();
+        //CoreMotionStatus CmStatus = new CoreMotionStatus();
+        //EngineStatus EnStatus = new EngineStatus();
+        Config.HomeParam AxisHomeParam = new Config.HomeParam();
+        Stopwatch stopWatch = new Stopwatch();
+        AdvancedMotion advmon = new AdvancedMotion();
+
+        int ErrorCode;
+        //PCCP Xavier Tsai, added for testing <END>
+
+        //PCCP Xavier Tsai, added for testing <START>
+        /// <summary>
+        /// ServoMotor WMX3 Control API
+        /// </summary>
+        /// 
+        public void WMX3_Initial()
+        {
+            //建立裝置
+            wmx.CreateDevice("C:\\Program Files\\SoftServo\\WMX3", DeviceType.DeviceTypeNormal, 10000);
+
+            //設定裝置名稱
+            wmx.SetDeviceName("DLF");
+
+            //設置齒輪比
+            int A = motion.Config.SetGearRatio(0, 1048576, 10000);
+            int B = motion.Config.SetGearRatio(1, 1048576, 10000);
+
+        }  //end of public void WMX3_Initial()
+
+        public int WMX3_establish_Commu()
+        {
+            int rslt = 0;
+
+            int ret = wmx.StartCommunication();
+            if (ret != 0)
+            {
+                string str = WMX3Api.ErrorToString(ret);
+                MessageBox.Show(str);
+            }
+
+            return rslt;
+        }  //end of public void WMX3_establish_Commu()
+
+        public void WMX3_destroy_Commu()
+        {
+            wmx.StopCommunication();
+        }  //end of public void WMX3_destroy_Commu()
+
+        public int WMX3_check_Commu()
+        {
+            int rslt = 0;
+
+            //讀取當前通訊狀態
+            motion.GetStatus(ref CmStatus);
+
+            switch (CmStatus.EngineState)
+            {
+                case EngineState.Running:
+                    rslt = 0;
+                    break;
+
+                case EngineState.Communicating:
+                    rslt = 1;
+                    break;
+            }
+
+            return rslt;
+        }  //end of public int WMX3_check_Commu()
+
+        public void WMX3_ServoOnOff(int axis, int bOn)
+        {
+            int newStatus = bOn;
+
+            //啟動伺服
+            int ret = motion.AxisControl.SetServoOn(axis, newStatus);
+
+            if (ret != 0)
+            {
+                string ers = CoreMotion.ErrorToString(ret);
+                MessageBox.Show($"{ers}");
+            }
+        }  //end of public void WMX3_ServoOn(int axis)
+
+        public int WMX3_check_ServoOnOff(int axis, ref string position, ref string speed)
+        {
+            int rslt = 0;
+
+            //讀取SV ON狀態
+            CoreMotionAxisStatus cmAxis = CmStatus.AxesStatus[axis];
+            if (cmAxis.ServoOn == true)
+            {
+                rslt = 1;
+            }
+            else
+            {
+                rslt = 0;
+            }
+
+            //讀取目前位置
+            string Profile = cmAxis.ActualPos.ToString();
+
+            //strtok info
+            position = Profile.Substring(0, Math.Min(Profile.Length, 6));
+            speed = cmAxis.ActualVelocity.ToString();
+            //AcTqr0.Text = cmAxis.ActualTorque.ToString();
+
+            return rslt;
+        }  //end of int WMX3_check_ServoOnOff(int axis)
+
+        public int WMX3_Pivot(int axis, int pivot, int speed, int accel, int daccel)
+        {
+            int rslt = 0;
+
+            //位置控制設定
+            int ret1 = motion.AxisControl.SetAxisCommandMode(0, AxisCommandMode.Position);
+
+            //POS參數設置
+            Motion.PosCommand pos = new Motion.PosCommand();
+
+            pos.Profile.Type = ProfileType.Trapezoidal;  //運動模式
+            pos.Axis = axis;    //軸
+            pos.Target = pivot;   //指定位置
+            pos.Profile.Velocity = speed;   //速度
+            pos.Profile.Acc = accel;   //加速度
+            pos.Profile.Dec = daccel;  //減速度
+
+            //啟動POS運轉
+            rslt = motion.Motion.StartPos(pos);
+
+            if (rslt != 0)
+            {
+                string ers = CoreMotion.ErrorToString(rslt);  //如果無法通訊則報錯誤給使用者
+                //   textBox12.Text += "軸" + textBox3.Text + ":" + ers + "\r\n";
+            }
+
+            return rslt;
+        }  //end of public void WMX3_Pivot(int pivot, int speed, int accel, int daccel)
+
+        public int WMX3_SetHomePosition(int axis)
+        {
+            int rslt = 0;
+
+            //讀取當前座標
+            AxisHomeParam.HomeType = Config.HomeType.CurrentPos;
+
+            //設置原點參數
+            rslt = motion.Config.SetHomeParam(axis, AxisHomeParam);
+
+            if (rslt != 0)
+            {
+                string ers = CoreMotion.ErrorToString(rslt);//如果無法通訊則報錯誤給使用者
+                //   textBox12.Text += "軸" + textBox9.Text + "設置HOME錯誤" + ers + "\r\n";
+            }
+
+            //開始回原點
+            rslt = motion.Home.StartHome(axis);
+
+            return rslt;
+        }  //end of public int WMX3_SetHomePosition(int axis)
+
+        private void btn_DeviceCreate_Click(object sender, EventArgs e)
+        {
+            Wmx3Lib.CreateDevice("C:\\Program Files\\SoftServo\\WMX3\\", DeviceType.DeviceTypeNormal, 100000);
+            Wmx3Lib.SetDeviceName("MotorControl");
+            label1.Text = "創建無異常";
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            int ret = Wmx3Lib.StartCommunication();//WMX3通訊啟動
+            if (ret != 0)
+            {
+
+                string str = WMX3Api.ErrorToString(ret); //如果無法通訊則報錯誤給使用者
+                label1.Text = "裝置連線失敗-" + str;
+            }
+            else
+            {
+                label1.Text = "裝置已連線";
+                //Main_Timer.Enabled = true;
+
+                //Thread TR_M_Status_Read = new Thread(M_Status_Read);
+                //TR_M_Status_Read.IsBackground = true;
+                //TR_M_Status_Read.Start();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Stop Communication.
+            Wmx3Lib.StopCommunication(10000);
+            //Quit device.
+            Wmx3Lib.CloseDevice();
+            Wmx3Lib_cm.Dispose();
+            Wmx3Lib.Dispose();
+
+            //sw.Close();
+        }
+
+        private void btn_AlarmRST_Click(object sender, EventArgs e)
+        {
+            Wmx3Lib_cm.AxisControl.ClearAmpAlarm((int)NUD_Motor_NO.Value);
+        }
+        //PCCP Xavier Tsai, added for testing <END>
+
+
+
     }
 }
